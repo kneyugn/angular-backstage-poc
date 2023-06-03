694ea520d17a93c1e0423a20d33795616d71486a
kneyugn <kneyugn@users.noreply.github.com>
2023-06-03 16:35:31 -0400

    feat: working poc with 8f5e91e00ecd7356be479c2000de787a044860f4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools'
+const routes: Routes = [
+  {
+        path: 'gha',
+        component: WebComponentWrapper,
+        data: {
+          type: 'script',
+          remoteEntry: 'http://localhost:3023/remoteEntry.js',
+          exposedModule: './gha',
+          remoteName: 'gha',
+          elementName: 'gha-react-element',
+        } as WebComponentWrapperOptions
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
